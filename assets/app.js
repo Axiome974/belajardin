d600ca7db239ffc83aa43db894526cf22ef4a548
Damien Carreno <damien@oclock.io>
2022-03-07 13:24:19 +0100

    template integrated

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'animate.css/animate.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'boxicons/css/boxicons.min.css';
+import './template_assets/css/style.css';
+import './styles/app.css';
+
+// Modules
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'glightbox/dist/js/glightbox.min.js';
+import 'isotope-layout/js/isotope.js';
+import 'swiper';
+
+// Js files
+import './template_assets/js/php-email-form/validate.js';
+import './template_assets/js/purecounter/purecounter.js';
+import './template_assets/js/main.js';
+
+
+// start the Stimulus application
+import './bootstrap';
---
+++ src/Controller/Webstite/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Webstite;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route ("/", name="home_")
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('webstite/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}

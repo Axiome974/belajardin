a16efc3eeb017f5819d7d44190781afe7ce9d06f
Damien Carreno <damien@oclock.io>
2022-03-07 14:42:45 +0100

    Links for BO and website

---
+++ src/Service/AsideMenuService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class AsideMenuService{
+
+    const ELEMENTS = [
+        "Accueil"   => "backoffice_main",
+    ];
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param RequestStack $request
+     */
+    public function __construct( UrlGeneratorInterface $urlGenerator, RequestStack $request)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->request = $request;
+    }
+
+    public function getRender(){
+
+        $activeRoute = $this->request->getCurrentRequest()->get("_route");
+        $html = "";
+
+
+        foreach ( self::ELEMENTS as $label => $link ){
+            $activeClass = ( $activeRoute === $link ? "active" : "text-white" );
+
+            if( $link === "" || $link === "#"){
+                $route = "#";
+            }else{
+                $route = $this->urlGenerator->generate($link);
+            }
+            $html .= "
+                <li class='nav-item'>
+                    <a href='#' class='nav-link ${activeClass}' aria-current='page'>
+                    <svg class='bi me-2' width='16' height='16'><use xlink:href='${route}'></use></svg>
+                     ${label}
+                    </a>
+                </li>
+            ";
+        }
+        return $html;
+    }
+
+}
